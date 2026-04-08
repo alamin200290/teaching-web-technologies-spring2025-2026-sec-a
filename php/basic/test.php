2d111045d3bc54187d45041b4bf4abc783ab5c12
alamin200290 <amin.se.aiub@gmail.com>
2026-04-08 16:02:58 +0600

    php intro

---
+++ php/basic/test.php
@@ -0,0 +1,51 @@
+
+<?php
+    //$name = "alamin";
+//     $id = 12;
+//     $cgpa = 3.5;
+
+//     $std = [1, 'alamin', 2.5];
+//     $std = array(1, 'alamin', 4);
+//     $std[2];
+
+//     $stds = [
+//         [1, 'alamin', 2.5],
+//         [1, 'alamin', 2.5],
+//         [1, 'alamin', 2.5]
+//     ];
+//     $std[2][2];
+
+//     $std = ['id'=>1, 'name'=>'alamin', 'cgpa'=>2.5];
+//     $std['cgpa'];
+
+//     $stds = [
+//         's1'=>['id'=>1, 'name'=>'alamin', 'cgpa'=>2.5],
+//         's2'=>['id'=>2, 'name'=>'xyz', 'cgpa'=>2.2],
+//         's3'=>['id'=>3, 'name'=>'abc', 'cgpa'=>2.3]
+//     ];
+
+//    $stds['s2']['name'];
+
+//     //$std = [1, 'alamin', 2.5];
+        for($a=0; $a<10; $a++){
+            //echo "<h1>TEST</h1>";
+        }
+//     foreach($std as $v){
+//             $v;
+//     }
+
+//     if(){
+
+//     }else{
+
+//     }
+
+
+    //echo "Hello PHP {$name} another test....";
+    //print("Your name is: {$name} ");
+    //$std = ['id'=>1, 'name'=>'alamin', 'cgpa'=>2.5];
+    //print_r($std);
+    //var_dump($std);
+
+?>
+
